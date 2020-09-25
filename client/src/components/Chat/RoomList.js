f6c85d3402ad3c32a10a7b15dc4aadb0a73a6e8b
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-25 12:56:28 -0400

    Finished roomlist array function.

--- client/src/components/Chat/RoomList.js
+++ client/src/components/Chat/RoomList.js
@@ -28,3 +28,16 @@ function RoomList() {
                 setShowLoading(false);
             });
         };
+        
+        fetchData();
+    }, []); 
+    const snapshotToArray = (snapshot) => {
+        const returnArr = [];
+
+        snapshot.forEach((childSnapshot) => {
+            const item = childSnapshot.val();
+            item.key = childSnapshot.key;
+            returnArr.push(item);
+        });
+
+        return returnArr;
