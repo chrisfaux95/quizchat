01ba486c536d512117c721990d01a53f6c586cc5
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-25 12:50:35 -0400

    Started Roomlist function.

---
+++ client/src/components/Chat/RoomList.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Link,
+    useHistory
+  } from "react-router-dom";
+import {
+    Jumbotron,
+    Spinner,
+    ListGroup,
+    ListGroupItem,
+    Button
+} from 'reactstrap';
+import Moment from 'moment';
+import firebase from '../Firebase';
+
+function RoomList() {
+    const [room, setRoom] = useState([]);
+    const [showLoading, setShowLoading] = useState(true);
+    const [nickname, setNickname] = useState('');
+    const history = useHistory();
