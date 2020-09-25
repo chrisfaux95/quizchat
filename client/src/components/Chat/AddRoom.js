07afc2d571e103dee67a241634ab3baae4a63ce2
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-25 12:34:27 -0400

    Save function for rooms added.

--- client/src/components/Chat/AddRoom.js
+++ client/src/components/Chat/AddRoom.js
@@ -18,3 +18,24 @@ import React, { useState } from 'react';
         const [room, setRoom] = useState({ roomname: '' });
         const [showLoading, setShowLoading] = useState(false);
         const ref = firebase.database().ref('rooms/');
+
+        const save = (e) => {
+            e.preventDefault();
+            setShowLoading(true);
+            ref.orderByChild('roomname').equalTo(room.roomname).once('value', snapshot => {
+                if (snapshot.exists()) {
+                    return (
+                        <div>
+                            <Alert color="primary">
+                                Room name already exist!
+                            </Alert>
+                        </div>
+                    );
+                } else {
+                    const newRoom = firebase.database().ref('rooms/').push();
+                    newRoom.set(room);
+                    history.goBack();
+                    setShowLoading(false);
+                }
+            });
+        };
