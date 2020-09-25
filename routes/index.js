7d75a71cede25cc7797993f28011efc68ac95691
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-25 12:17:04 -0400

    added react home path

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.html"))
+})
+
 module.exports = router;
