958a248229ec228a2320bd2f4364f55ab0daf08c
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-25 12:16:18 -0400

    Named const and values for users and declared arrays.

---
+++ client/src/components/Chat/ChatRoom.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import {
+    useHistory,
+    useParams
+  } from "react-router-dom";
+import {
+    Container, 
+    Row, 
+    Col,
+    Card,
+    CardBody,
+    CardSubtitle,
+    Button,
+    Form,
+    InputGroup,
+    Input,
+    InputGroupAddon
+} from 'reactstrap';
+import Moment from 'moment';
+import firebase from '../Firebase';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import '../style/Styles.css';
+
+function ChatRoom(props) {
+    const [chats, setChats] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [nickname, setNickname] = useState('');
+    const [roomname, setRoomname] = useState('');
+    const [newchat, setNewchat] = useState({ roomname: '', nickname: '', message: '', date: '', type: '' });
+    const history = useHistory();
+    const { room } = useParams();
