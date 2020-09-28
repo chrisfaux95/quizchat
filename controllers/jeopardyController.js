71b85e11f87bbd6e8e11291ec830bb7584623d41
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-28 11:29:11 -0400

    added findById and findAll for jeopardy db

---
+++ controllers/jeopardyController.js
@@ -0,0 +1,16 @@
+const db = require("../models");
+
+module.exports = {
+    findById: (req, res) => {
+        db.Jeopardy
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findAll: (req, res) => {
+        db.Jeopardy
+            .findById(res.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+}
