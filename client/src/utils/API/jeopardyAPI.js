a9c74e0c42fe1ede17a3e9433b8aef0f591995d8
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-29 12:53:12 -0400

    added getAll and getById

--- client/src/utils/API/jeopardyAPI.js
+++ client/src/utils/API/jeopardyAPI.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 export default {
     getRandom: function () {
-        
+        return axios.get("/api/jeopardy/random");
+    },
+    getAll: function (req) {
+        return axios.get("/api/jeopardy", req);
+    },
+    getById: function (id) {
+        return axios.get("/api/jeopardy/byID/" + id);
     }
 }
