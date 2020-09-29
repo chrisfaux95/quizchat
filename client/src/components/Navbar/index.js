57fccf41ad213db539238b0be94e9e29800af26f
Frank Buono <fbuonojr@fordham.edu>
2020-09-29 11:22:31 -0400

    copied logo image into folder

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,46 +1,47 @@
 import React from "react";
+import { Nav } from "reactstrap";
 
-function Navbar() {
+function Navigation() {
     return (
-        <nav class="mb-1 navbar navbar-expand-lg navbar-light quiz-page">
-            <a href="./assets/images/Triviatastic.png" class="pull-left"><img src="./assets/images/Triviatastic.png"
+        <nav  className="mb-1 navbar navbar-expand-lg navbar-light quiz-page">
+            <a href="./assets/images/Triviatastic.png"  className="pull-left"><img src="./assets/images/Triviatastic.png"
                 style="height:50px;"/></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-555"
+                <button  className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-555"
                     aria-controls="navbarSupportedContent-555" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span  className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent-555">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="/landing">Home
-            <span class="sr-only">(current)</span>
+                <div  className="collapse navbar-collapse" id="navbarSupportedContent-555">
+                    <ul  className="navbar-nav mr-auto">
+                        <li  className="nav-item active">
+                            <a  className="nav-link" href="/landing">Home
+            <span  className="sr-only">(current)</span>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/hold">Quiz</a>
+                        <li  className="nav-item">
+                            <a  className="nav-link" href="/hold">Quiz</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/signup">Sign Up</a>
+                        <li  className="nav-item">
+                            <a  className="nav-link" href="/signup">Sign Up</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/login">Login</a>
+                        <li  className="nav-item">
+                            <a  className="nav-link" href="/login">Login</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/members">User Score</a>
+                        <li  className="nav-item">
+                            <a  className="nav-link" href="/members">User Score</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/highscores">Highscore</a>
+                        <li  className="nav-item">
+                            <a  className="nav-link" href="/highscores">Highscore</a>
                         </li>
                     </ul>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true"
+                    <li  className="nav-item dropdown">
+                        <a  className="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown" aria-haspopup="true"
                             aria-expanded="false">
-                            <i class="fas fa-user"></i>
+                            <i  className="fas fa-user"></i>
                         </a>
-                        <div class="dropdown-menu dropdown-menu-right dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
-                            <a id="login-nav" class="dropdown-item" href="/login">Log In</a>
-                            <a id="signup-nav" class="dropdown-item" href="/signup">Sign Up!</a>
-                            <a id="logout-nav" class="dropdown-item" href="/logout">Logout</a>
+                        <div  className="dropdown-menu dropdown-menu-right dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
+                            <a id="login-nav"  className="dropdown-item" href="/login">Log In</a>
+                            <a id="signup-nav"  className="dropdown-item" href="/signup">Sign Up!</a>
+                            <a id="logout-nav"  className="dropdown-item" href="/logout">Logout</a>
                         </div>
                     </li>
                 </div>
@@ -48,3 +49,5 @@ function Navbar() {
 
     )
 }
+
+export default Navigation;
