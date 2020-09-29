2b69fa71bd2ac29e7777730b23db3db45e1e5ed4
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-29 12:16:28 -0400

    added API files

---
+++ client/src/utils/API/index.js
@@ -0,0 +1,11 @@
+import jeopardyAPI from './jeopardyAPI';
+import jeopardyAPI from './jeopardyAPI';
+import otdbAPI from "./otdbAPI";
+import scoresAPI from "./scoresAPI"
+
+
+export default {
+    otdb: otdbAPI,
+    jeopardy: jeopardyAPI,
+    scores: scoresAPI
+}
