65285120a1de485939d326fd1f56053021673fb8
Frank Buono <fbuonojr@fordham.edu>
2020-09-29 12:36:29 -0400

    finished game template page

--- client/src/pages/Quiz/index.js
+++ client/src/pages/Quiz/index.js
@@ -2,27 +2,43 @@ import React from "react";
 import {
     Card,
     CardColumns,
-    CardImg, 
-    CardText, 
-    CardBody, 
-    CardTitle, 
-    CardSubtitle, 
-    Button
+    CardImg,
+    CardText,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    Button,
+    Jumbotron,
+    Container
 } from 'reactstrap';
 import "./style.css"
-function QuizPg (props) {
-        return (
-          <div>
+function QuizPg(props) {
+    return (
+        <div className="row">
             <Card className="col-lg-3">
-              <CardImg className="cardLogo" top width="100%" src="./Triviatastic.png" alt="Card image cap" />
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-              </CardBody>
+                {/* <CardImg className="cardLogo" top width="100%" src="./Triviatastic.png" alt="Card image cap" /> */}
+                <CardBody>
+                    <CardTitle><h1>Scoreboard</h1></CardTitle>
+                    <hr />
+                    <CardSubtitle><h3>Username</h3></CardSubtitle>
+                    <hr />
+                    <CardText>Score</CardText>
+                    <hr />
+                    <CardText>Highcore</CardText>
+                    <hr />
+                    <CardText>Your Answer</CardText>
+                    <hr />
+                    <CardText>Correct Answer</CardText>
+                    </CardBody>
             </Card>
-          </div>
-        );
+            <Jumbotron className="col-lg-9 my-0" fluid>
+                <Container fluid>
+                    <h3 className="display-3">Dump Game Here</h3>
+                    <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                </Container>
+            </Jumbotron>
+        </div>
+    );
 };
 
 export default QuizPg;
