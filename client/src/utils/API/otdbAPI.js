240463f0c46512f54633359a4b1959f223dfff83
chrisfaux95 <chrisfaux95@gmail.com>
2020-10-01 16:21:58 -0400

    fixed API

--- client/src/utils/API/otdbAPI.js
+++ client/src/utils/API/otdbAPI.js
@@ -1,46 +1,55 @@
 import axios from "axios";
+const BASEURL_count = "https://opentdb.com/api_count.php?"
+const BASEURL_qs = "https://opentdb.com/api.php?";
+
 
 export default {
     getQuiz: function (catNum, difficulty) {
-        let qAmt = this.getQAmt(catNum, difficulty);
-        let qURL = `https://opentdb.com/api.php?amount=${qAmt}`;
-        qURL += `&category=${catNum}`;
-        
-        return axios.get(qURL);
+        let qURL = `${BASEURL_count}category=${catNum}`
+        // console.log(qURL)
+        axios.get(qURL).then(res => {
+            var qAmt = 0;
+            let qTot
+            console.log(res);
+            switch (difficulty) {
+                case 'easy':
+                    qTot = res.data.category_question_count.easy_question_count;
+                    qAmt = qTot < 10 ? qTot : 10;
+                    break;
+                case 'medium':
+                    qTot = res.data.category_question_count.medium_question_count;
+                    qAmt = qTot < 10 ? qTot : 10;
+                    break;
+                case 'hard':
+                    qTot = res.data.category_question_count.hard_question_count;
+                    qAmt = qTot < 10 ? qTot : 10;
+                    break;
+                case 'any':
+                default:
+                    qTot = res.data.category_question_count.total_question_count;
+                    qAmt = qTot < 10 ? qTot : 10;
+            }
+            let qURL = `${BASEURL_qs}amount=${qAmt}`;
+            qURL += `&category=${catNum}`;
+            console.log(qURL)
+            return axios.get(qURL);
+        })
     },
     getQuizAny: function (difficulty) {
         let qAmt = 10;
-        let qURL = `https://opentdb.com/api.php?amount=${qAmt}`;
+        let qURL = `${BASEURL_qs}amount=${qAmt}`;
         if (difficulty != 'any') qURL += `&difficulty=${difficulty}`;
-
+        console.log(qURL);
         return axios.get(qURL);
     },
-    getQAmt: function (catNum, difficulty) {
-        var qAmt;
-        let qTot
-        axios.get(`https://opentdb.com/api_count.php?category=${catNum}`).then(
-            res => {
-                switch (difficulty) {
-                    case 'easy':
-                        qTot = res.category_question_count.easy_question_count;
-                        qAmt = qTot < 10 ? qTot : 10;
-                        break;
-                    case 'medium':
-                        qTot = res.category_question_count.medium_question_count;
-                        qAmt = qTot < 10 ? qTot : 10;
-                        break;
-                    case 'hard':
-                        qTot = res.category_question_count.hard_question_count;
-                        qAmt = qTot < 10 ? qTot : 10;
-                        break;
-                    case 'any':
-                    default:
-                        qTot = res.category_question_count.total_question_count;
-                        qAmt = qTot < 10 ? qTot : 10;
-                }
-            }
-        )
-        return qAmt;
+    getQuizAsync: async function (catNum, diff) {
+        try {
+            let qAmtRes = await axios.get(BASEURL_count + 'amount=' + catNum)
+            let qAmt
+            let result = await axios.get
+        } catch (err) {
+            console.log(err)
+        }
     }
 
 }
