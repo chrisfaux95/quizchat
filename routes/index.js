95435e729fb407e22a8e5d3a208b2aa202485492
chrisfaux95 <chrisfaux95@gmail.com>
2020-10-05 15:17:18 -0400

    heroku #724

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../client/index.html"))
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
 })
 
 module.exports = router;
