ab38728122fd5bef55de8d4d5a66d101b62349f5
chrisfaux95 <chrisfaux95@gmail.com>
2020-10-05 15:26:36 -0400

    added input

--- client/src/components/Quiz/FinalScore.js
+++ client/src/components/Quiz/FinalScore.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Button } from 'reactstrap'
+import { Button, Input } from 'reactstrap'
 import QuizContext from '../../utils/QuizContext';
 
 export default function FinalScore(props){
@@ -8,11 +8,18 @@ export default function FinalScore(props){
     const handleReset = function () {
         quiz.handleScoreSubmit();
     }
+
+    const handleScoreSave = function () {
+        
+    }
+
     return (
         <>
             <h1>COMPLETE</h1>
             <h4>You scored {quiz.score} points!</h4>
             <Button onClick={handleReset}>Retry</Button>
+            <Input />
+            <Button onClick={handleScoreSave}>Save Your Score</Button>
         </>
     )
 }
