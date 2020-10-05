97c7f7697a83c527315bf4208dcb609ee979f572
chrisfaux95 <chrisfaux95@gmail.com>
2020-10-05 10:59:19 -0400

    expanding context functions

--- client/src/utils/QuizContext.js
+++ client/src/utils/QuizContext.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import Quiz from '../components/Quiz';
+import { getQuestions, shuffleArray } from './quizFunctions';
 
 const QuizContext = React.createContext({
     stage: 0,
     score: 0,
-    questions: []
-    onClick:
+    questions: [],
+    currentQ: "",
+    cat: 0,
+    diff: "",
+    handleForm: function (cat, diff) {
+        this.cat = cat;
+        this.diff = diff;
+        let q = await getQuestions(diff, cat);
+        shuffleArray(q);
+        this.questions = [...q];
+        this.currentQ = this.questions[0];
+        this.stage = 1;
+    },
+    handleQuiz: function (answer) {
+        if (answer === this.currentQ.correct_answer) {
+            this.score += 100;
+        }
+        let nextQindex = this.questions.indexOf(this.currentQ) + 1;
+        if (nextQindex >= this.questions.length) {
+            this.stage = 2;
+        }
+    },
+    handleScoreSubmit: function (username) {
+        // SUBMIT SCORE HERE
+        this.stage = 0;
+    }
 });
+
 
 
 
