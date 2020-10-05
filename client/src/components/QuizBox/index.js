696a9a340e3c35c14f8118cf6a0890f1c0a75d9d
chrisfaux95 <chrisfaux95@gmail.com>
2020-10-05 10:08:58 -0400

    general quiz construction

--- client/src/components/QuizBox/index.js
+++ client/src/components/QuizBox/index.js
@@ -1,58 +1,54 @@
 import React, { useState } from 'react';
-import { Button, Container} from 'reactstrap';
+import { Button, Container } from 'reactstrap';
 import { shuffleArray } from '../../utils/quizFunctions';
+import { CatHead, QHead, AnsBtn } from '../Quiz/questionComponents';
 
 export default function QuizBox(props) {
-    let { q } = props;
+    let { q, score, setScore, handleQuizState } = props;
     const [qIndex, setQIndex] = useState(0);
-    const [questions, setQuestions] = useState({...q});
+    const [questions, setQuestions] = useState([...q]);
+    const [currentQ, setCurrentQ] = useState(questions[qIndex]);
     const handleAnswer = () => {
         console.log(qIndex)
-        if (qIndex > questions.length){
-            setQIndex(0)
+        if (qIndex > questions.length) {
+            setQIndex(0);
+            handleQuizState();
         } else {
             setQIndex(qIndex + 1);
+            setScore(score + 1);
         }
     }
+
+    const handleAnswerDisplay = function(question){
+        console.log(question.correct_answer);
+        let ansArr = [question.correct_answer, ...question.incorrect_answers];
+        shuffleArray(ansArr);
+        // console.log(shuffleArray);
+        return (
+            <div>
+                {ansArr.map(a => <AnsBtn handleAnswer={handleAnswer} answer={a}/>)}
+            </div>
+        )
+    
+    }
+
     return (
         // <QuestionBox question={questions[qIndex]} handleAnswer={handleAnswer} />
         <div>
-            <h1>{questions[qIndex].category}</h1>
-            <h2>{questions[qIndex].question}</h2>
-            <ul>
-                <li>
-                    {questions[qIndex].correct_answer}
-                </li>
-                {questions[qIndex].incorrect_answers.map(i =>{
-                    return <li>{i}</li>
-                })}
-            </ul>
-            <Button onClick={handleAnswer}>NEXT QUESTION</Button>
+            {questions[qIndex] ? <>
+                <CatHead>
+                    {questions[qIndex].category}
+                </CatHead>
+                <QHead>
+                    {questions[qIndex].question}
+                </QHead>
+                <div>
+                    {handleAnswerDisplay(questions[qIndex])}
+                </div>
+                {questions[qIndex-1] ? <>The Correct Answer was: {questions[qIndex-1].correct_answer} </> : null}
+            </> : <>COMPLETE</>}
         </div>
     )
 }
 
 
-function QuizButton({ answer, onClick }) {
-    return (
-        <Button onClick={onClick}>
-            {answer}
-        </Button>
-    )
-}
-
-function QuestionBox(props) {
-    const { question, handleAnswer } = props;
-    const answers = [question.correct_answer, ...question.incorrect_answer];
-    shuffleArray(answers);
-    return (
-        <>
-            <h1>{ question.category }</h1>
-            <br />
-            <h4>{ question.question }</h4>
-            <br />
-            {answers.map((ans) => <QuizButton answer={ans} onClick={handleAnswer} />)}
-        </>
-    )
-
-}
